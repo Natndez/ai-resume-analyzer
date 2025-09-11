fb88adb73dd1c50ffacdb16bf105b41e37cfeb64
Nathan Fernandez <fernandeznathan891@gmail.com>
2025-09-11 15:45:36 -0400

    Set up navbar component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,6 +11,13 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-Powered feedback!</h2>
+      </div>
+    </section>
 
   </main>;
 }
