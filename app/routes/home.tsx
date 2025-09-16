212ad5215e5192105f96a2a038b9ba341dd84539
Nathan Fernandez <fernandeznathan891@gmail.com>
2025-09-16 14:24:19 -0400

    Added some extra padding and extra resumes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,7 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 import Navbar from "~/components/Navbar";
+// @ts-ignore
 import { resumes } from "constants";
 import ResumeCard from "~/components/ResumeCard";
 
@@ -15,12 +16,12 @@ export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar />
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Track Your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-Powered feedback!</h2>
       </div>
 
-    {resumes.length > 0 && (
+    {resumes?.length > 0 && (
       <div className="resumes-section">
         {/* Mapping over an array that contains different resumes */}
         {resumes.map((resume: Resume) => (
@@ -29,7 +30,5 @@ export default function Home() {
       </div>
     )}
     </section>
-
-
   </main>;
 }
